63d2441e8a35a3ffbc66294466f3d3cd6738812c
Alexandre ZANNI <16578570+noraj@users.noreply.github.com>
2020-03-04 00:49:18 +0100

    Update xss_chart.js

--- docs/js/xss_chart.js
+++ docs/js/xss_chart.js
@@ -1,7 +1,6 @@
 // Author: noraj (Alexandre ZANNI)
 var ctx = document.getElementById('xss');
 var ctx2 = document.getElementById('xss2');
-var ctx3 = document.getElementById('xss3');
 // XSS type labels
 var xonload = 'Onload';
 var interaction = 'Interaction';
@@ -9,26 +8,23 @@ var xself = 'Self';
 var dom = 'DOM-based';
 var reflected = 'Reflected';
 var stored = 'Stored';
-var universal = 'Universal';
-var blind = 'Blind';
-var mutated = 'Mutated';
-var p2p = 'p2p';
+var p2p = 'p2p (WebRTC)';
 // Graph colors
-var orangered = [
-  'hsla(16, 100%, 50%, 1)',
-  'hsla(16, 100%, 50%, 1)',
-  'hsla(16, 100%, 50%, 1)'
-]; // OrangeRed
-var tomato = [
-  'hsla(9, 100%, 64%, 1)',
-  'hsla(9, 100%, 64%, 1)',
-  'hsla(9, 100%, 64%, 1)'
-]; // Tomato
-var coral = [
-  'hsla(16, 100%, 66%, 1)',
-  'hsla(16, 100%, 66%, 1)',
-  'hsla(16, 100%, 66%, 1)'
-]; // Coral
+// var orangered = [
+//   'hsla(16, 100%, 50%, 1)',
+//   'hsla(16, 100%, 50%, 1)',
+//   'hsla(16, 100%, 50%, 1)'
+// ]; // OrangeRed
+// var tomato = [
+//   'hsla(9, 100%, 64%, 1)',
+//   'hsla(9, 100%, 64%, 1)',
+//   'hsla(9, 100%, 64%, 1)'
+// ]; // Tomato
+// var coral = [
+//   'hsla(16, 100%, 66%, 1)',
+//   'hsla(16, 100%, 66%, 1)',
+//   'hsla(16, 100%, 66%, 1)'
+// ]; // Coral
 var darkorange = [
   'hsla(33, 100%, 50%, 1)',
   'hsla(33, 100%, 50%, 1)',
@@ -61,12 +57,9 @@ var bgColor = 'rgb(24, 26, 27)';
 var data = {
   datasets: [{
     borderColor: bgColor,
-    data: [5, 5, 5, 5, 10, 10, 10, 10],
+    data: [5, 10, 10, 10, 10],
     label: xself,
     labels: [
-      universal,
-      mutated,
-      blind,
       p2p,
       stored,
       stored,
@@ -74,19 +67,16 @@ var data = {
       reflected
     ],
     backgroundColor: [
-      orangered[0], tomato[0], coral[0], darkorange[0],
+      darkorange[0],
       limegreen[0], limegreenbis[0],
       dodgerblue[0], dodgerbluebis[0]
     ]
   },
   {
     borderColor: bgColor,
-    data: [5, 5, 5, 5, 10, 10, 10, 10],
+    data: [5, 10, 10, 10, 10],
     label: interaction,
     labels: [
-      universal,
-      mutated,
-      blind,
       p2p,
       stored,
       stored,
@@ -94,19 +84,16 @@ var data = {
       reflected
   ],
     backgroundColor: [
-      orangered[1], tomato[1], coral[1], darkorange[1],
+      darkorange[1],
       limegreen[1], limegreenbis[1],
       dodgerblue[1], dodgerbluebis[1]
     ]
   },
   {
     borderColor: bgColor,
-    data: [5, 5, 5, 5, 10, 10, 10, 10],
+    data: [5, 10, 10, 10, 10],
     label: xonload,
     labels: [
-      universal,
-      mutated,
-      blind,
       p2p,
       stored,
       stored,
@@ -114,26 +101,20 @@ var data = {
       reflected
     ],
     backgroundColor: [
-      orangered[2], tomato[2], coral[2], darkorange[2],
+      darkorange[2],
       limegreen[2], limegreenbis[2],
       dodgerblue[2], dodgerbluebis[2]
     ]
   },
   ],
   labels: [
-    'Universal',
-    'Mutated',
-    'Blind',
-    'p2p',
+    'p2p (WebRTC)',
     'Stored',
     'DOM-based Stored',
     'Reflected',
     'DOM-based Reflected',
   ],
   dom: [
-    true,
-    true,
-    null,
     null,
     false,
     true,
@@ -336,10 +317,4 @@ var pieChart = new Chart(ctx2, {
   type: 'pie',
   data: data2,
   options: options2
-})
-
-var pieChart = new Chart(ctx3, {
-  type: 'pie',
-  data: data3,
-  options: options3
 })
